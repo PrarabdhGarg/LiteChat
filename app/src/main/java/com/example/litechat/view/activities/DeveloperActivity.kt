b4028fde6de0a2d66bfb9d5298abb368c14bdd99
99suyashsoni <45849297+99suyashsoni@users.noreply.github.com>
2019-02-16 16:43:27 +0530

    Developer Activity  Implemented

--- app/src/main/java/com/example/litechat/view/activities/DeveloperActivity.kt
+++ app/src/main/java/com/example/litechat/view/activities/DeveloperActivity.kt
@@ -1,37 +1,34 @@
-package com.example.litechat.view.fragments
+package com.example.litechat.view.activities
 
+import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.support.v4.app.Fragment
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import com.example.litechat.R
 import com.example.litechat.view.adapters.DeveloperAdapter
-import kotlinx.android.synthetic.main.fragment_developer.view.*
+import kotlinx.android.synthetic.main.activity_developer.*
 
-class FragmentDevelopers: Fragment() {
+class DeveloperActivity : AppCompatActivity() {
 
     private lateinit var viewAdapter: RecyclerView.Adapter<*>
     private lateinit var viewManager: RecyclerView.LayoutManager
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val view = inflater.inflate(R.layout.fragment_developer, container, false)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_developer)
 
         val nameAndImages = arrayListOf(Pair("Suyash Soni",R.drawable.suyash),Pair("Prarabdh Garg",R.drawable.prarabdh),
             Pair("Akshat Gupta",R.drawable.akshat),Pair("Ishita Aggrawal",R.drawable.ishita))
 
         val fnameAndImages = nameAndImages.shuffled()
 
-        viewManager = LinearLayoutManager(activity)
-        viewAdapter = DeveloperAdapter(fnameAndImages, context!!)
-        view.developerRecycler.apply {
+        viewManager = LinearLayoutManager(applicationContext)
+        viewAdapter = DeveloperAdapter(fnameAndImages, applicationContext)
+
+        developerRecycler.apply {
 
             layoutManager = viewManager
             adapter = viewAdapter
         }
-
-        return view
     }
 }
